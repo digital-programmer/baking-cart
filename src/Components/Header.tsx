4f21cb12bb3c8d2aec30d6acbe967409c4ac1b3d
dibya-sunstone <dibya.acharya@sunstone.edu.in>
2022-12-06 09:05:40 +0530

    responsive

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,8 +4,8 @@ import { Cart } from "./Cart";
 export function Header() {
     return (
         <div className="bg-purple-900 fixed top-0 w-full z-10">
-            <div className="mx-auto flex items-center justify-between max-w-2xl py-8 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="text-3xl font-bold tracking-tight text-purple-100">Baking Good</h2>
+            <div className="mx-auto flex items-center justify-between max-w-2xl md:py-8 md:px-4 py-4 px-4 lg:max-w-7xl lg:px-8">
+                <h2 className="lg:text-3xl md:text-2xl text-xl font-bold tracking-tight text-purple-100">Baking Good</h2>
                 <div className="flex items-center lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
                     <Cart/>
                 </div>
--- src/Utils/Spacer.tsx
+++ src/Utils/Spacer.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export function Spacer () {
     return (
-        <div className='h-28 w-full'></div>
+        <div className='h-20 md:h-28 w-full'></div>
     )
 }
